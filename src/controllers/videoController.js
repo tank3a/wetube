3ed96ea5a1a5ba572599753176e7506fe5130d05
김종원 <tank3a@gmail.com>
2021-07-20 14:27:32 +0900

    added pug files & url parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
-export const recommended = (req, res) => res.send("Recommended");
+export const recommended = (req, res) => res.render("home", {pageTitle: "Home"});
 export const search = (req, res) => res.send("Search Video");
 
-export const see = (req, res) => res.send("See Video");
-export const edit = (req, res) => res.send("Edit Video");
-export const remove = (req, res) => res.send("Delete Video");
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
+export const remove = (req, res) => res.render("remove", {pageTitle: "Remove"});
 export const upload = (req, res) => res.send("Upload Video");
