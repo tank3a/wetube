de89e6b9843201c99c53b401a9c6c2f227f89ef8
김종원 <tank3a@gmail.com>
2021-07-22 11:29:35 +0900

    Added Mixin-Video, conditionals for login

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,37 @@
-export const recommended = (req, res) => res.render("home", {pageTitle: "Home"});
+const fakeUser = {
+    username: "Jongwon",
+    loggedIn: true,
+};
+
+export const recommended = (req, res) => {
+    const videos = [
+        {
+            title: 'First Video',
+            rating: 5,
+            comments: 2,
+            createdAt: "2 months ago",
+            views: 59,
+            id: 1
+        },
+        {
+            title: 'Second Video',
+            rating: 5,
+            comments: 2,
+            createdAt: "2 months ago",
+            views: 59,
+            id: 2
+        },
+        {
+            title: 'Third Video',
+            rating: 5,
+            comments: 2,
+            createdAt: "2 months ago",
+            views: 59,
+            id: 3
+        }
+    ];
+    return res.render("home", {pageTitle: "Home", fakeUser: fakeUser, videos });
+}
 export const search = (req, res) => res.send("Search Video");
 
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
