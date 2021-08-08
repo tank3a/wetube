b67286615bafcbb9c9052ab1a5f1ca5d8c0eba4e
김종원 <tank3a@gmail.com>
2021-08-08 19:56:45 +0900

    Created edit-profile page - code challenge

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -116,7 +116,30 @@ export const finishGithubLogin = async (req, res) => {
         return res.redirect("/login");
     }
 };
-export const edit = (req, res) => res.send("Edit User");
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", {pageTitle:"Edit Profile"});
+}
+export const postEdit = async (req, res) => {
+    const {
+        session: {
+            user: {_id},
+        },
+        body: {username, name, location},
+    }=req;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(_id, {
+        username, name, location,
+        },
+        {new: true});
+        req.session.user = updatedUser;
+        return res.redirect("/users/edit");
+    } catch {
+        if(await User.exists({username})) {
+            return res.status(400).render("edit-profile", {pageTitle:"Join", errorMessage:"Sorry, this username exists"});
+        }
+    }
+}
+
 export const logout = (req, res) => {
     req.session.destroy();
     return res.redirect("/");
